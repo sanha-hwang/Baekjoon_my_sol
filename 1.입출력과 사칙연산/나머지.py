59868cca739fec7f5eccb0f93a5063e44d37112e
sanha <sanha805@naver.com>
2021-08-29 17:56:43 +0900

    입출력과 사칙연산_sol

---
+++ 1.입출력과 사칙연산/나머지.py
@@ -0,0 +1,16 @@
+"""
+문제
+두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+
+입력
+두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+"""
+A, B, C = input("").split()
+A = int(A)
+B = int(B)
+C = int(C)
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
